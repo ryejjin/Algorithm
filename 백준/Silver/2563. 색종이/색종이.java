030a27acd052f71788f7c93744e310dcd3d66788
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-01-29 13:21:59 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        boolean[][] arr = new boolean[101][101];
+        int res = 0;
+
+        for (int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            for (int x=s; x<s+10; x++){
+                for (int y=e; y<e+10; y++){
+                    if (!arr[x][y]){
+                        arr[x][y] = true;
+                        res ++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+}
