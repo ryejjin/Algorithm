750a405b05bbda4d3900b809c7095cd16d4ace7f
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-02-01 19:38:03 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,33 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int arr[] = new int[n];
-
-        for (int i=0; i<n; i++){
+        // Pi 값들 입력 받기
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        //배열을 오름차순으로 정렬
         Arrays.sort(arr);
-
-        int res = 0;
-
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n-i; j++){
-                res += arr[j];
-            }
+        int sum = 0;  //총 합
+        int add = 0;  //더할 수
+        for (int i = 0; i < n; i++) {
+            add += arr[i];
+            sum += add;
         }
-
-        System.out.println(res);
-
+        System.out.print(sum);
     }
 }
