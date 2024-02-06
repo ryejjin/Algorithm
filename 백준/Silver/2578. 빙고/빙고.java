541b1ff9ec43b67b1b651426a75a24585ca33760
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-02-06 17:20:12 +0900

    [Silver IV] Title: 빙고, Time: 164 ms, Memory: 16256 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] bingo;
+    static int cnt;
+    static int turn = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bingo = new int[5][5];
+
+        // 빙고 입력
+        for (int i=0; i<5; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<5; j++){
+                bingo[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i=0; i<5; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<5; j++){
+                int num = Integer.parseInt(st.nextToken());
+                for (int x=0; x<5; x++){
+                    for (int y=0; y<5; y++){
+                        if (bingo[x][y] == num){
+                            bingo[x][y] = 0;
+                        }
+                    }
+                }
+                chk_col();
+                chk_row();
+                chk_right();
+                chk_left();
+
+                if (cnt>=3){
+                    System.out.println(turn);
+                    System.exit(0);
+                }
+
+                cnt = 0;
+                turn ++;
+            }
+        }
+    }
+    public static void chk_col(){
+        for (int i=0; i<5; i++){
+            int count = 0;
+            for (int j=0; j<5; j++){
+                if (bingo[i][j] == 0){
+                    count ++;
+                }
+                if (count == 5){
+                    cnt ++;
+                }
+            }
+        }
+    }
+    public static void chk_row(){
+        for (int i=0; i<5; i++){
+            int count = 0;
+            for (int j=0; j<5; j++){
+                if (bingo[j][i] == 0){
+                    count ++;
+                }
+                if (count == 5){
+                    cnt ++;
+                }
+            }
+        }
+    }
+    public static void chk_right(){
+        int count = 0;
+        for (int i=0; i<5; i++){
+            if (bingo[i][4-i] == 0){
+                count ++;
+            }
+            if (count == 5){
+                cnt ++;
+            }
+        }
+    }
+    public static void chk_left(){
+        int count = 0;
+        for (int i=0; i<5; i++){
+            if (bingo[i][i] == 0){
+                count ++;
+            }
+            if (count == 5){
+                cnt ++;
+            }
+        }
+    }
+}
