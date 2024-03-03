82bc41a515cafe9ad9148d42fe66b2320ed5ec13
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-03-03 14:02:17 +0900

    [Gold V] Title: 전구와 스위치, Time: 148 ms, Memory: 119604 KB -BaekjoonHub

---
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -0,0 +1,28 @@
+n = int(input())
+current = list(map(int, input()))
+goal = list(map(int, input()))
+
+def switch(now, next):
+    temp = now[:]
+    cnt = 0
+    for i in range(1, n):
+        if temp[i-1] == next[i-1]:
+            continue
+        cnt += 1
+        for j in range(i-1, i+2):
+            if j<n:
+                temp[j] = 1-temp[j]
+    if temp == next:
+        return cnt
+    else:
+        return 1e9
+
+res = switch(current, goal)
+current[0] = 1-current[0]
+current[1] = 1-current[1]
+res = min(res, switch(current, goal)+1)
+
+if res != 1e9:
+    print(res)
+else:
+    print(-1)
