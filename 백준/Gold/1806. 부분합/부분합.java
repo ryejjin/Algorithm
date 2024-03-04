caadf3339609b63b4cd35baa89edc50b75a67506
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-03-04 17:06:43 +0900

    [Gold IV] Title: 부분합, Time: 300 ms, Memory: 24444 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, s;
+    static int[] arr, dp;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        dp = new int[n];    // 누적합
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (i==0){
+                dp[i] = arr[i];
+            }else {
+                dp[i] = arr[i]+dp[i-1];
+            }
+        }
+
+        int left = 0;
+        int right = 1;
+        int res = Integer.MAX_VALUE;
+
+        while (right <= n){
+            if (left >= n) break;
+            int diff = dp[right-1] - dp[left] + arr[left];
+            if (diff >= s){
+                res = Math.min(res, (right-left));
+                left ++;
+            } else right++;
+        }
+
+        System.out.println(res == Integer.MAX_VALUE? 0 : res);
+
+    }
+}
