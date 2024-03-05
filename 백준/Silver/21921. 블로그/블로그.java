619610ad30ab815a744b9d5ebb4f89cb8e5734df
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-03-05 15:17:51 +0900

    [Silver III] Title: 블로그, Time: 396 ms, Memory: 35600 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,44 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, x, maxVisit, cnt;
-    static int[] visit, dp;
-    public static void main(String[] args) throws IOException{
+    static int[] visit;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
 
+        // 입력 배열
         visit = new int[n];
-        dp = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             visit[i] = Integer.parseInt(st.nextToken());
-            if (i==0){
-                dp[i] = visit[i];
-            }else dp[i] = dp[i-1]+visit[i];
         }
-
-        int start = 0;
-        int end = start+x-1;
-        while (start<=n-x){
-            if (end>n) break;
-            int diff = dp[end]-dp[start]+visit[start];
-//            System.out.println(start + " " + end + " " + diff);
-            if (diff > maxVisit){
-                maxVisit = diff;
+        int sum = 0;
+        for (int i=0; i<x; i++){
+            sum += visit[i];
+        }
+        maxVisit = sum;
+        cnt = 1;
+        for (int i=x; i<n; i++){
+            sum += (visit[i]-visit[i-x]);
+            if (maxVisit == sum){
+                cnt ++;
+            } else if (maxVisit<sum) {
+                maxVisit = sum;
                 cnt = 1;
-                start++;
-                end++;
-            } else if (diff == maxVisit) {
-                cnt += 1; start++; end++;
-            }else {
-                start++;
-                end++;
             }
         }
+
         System.out.println(maxVisit!=0? maxVisit+"\n"+cnt : "SAD");
     }
 }
