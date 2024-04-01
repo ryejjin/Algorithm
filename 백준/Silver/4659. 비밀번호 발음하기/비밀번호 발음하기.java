f0f6c6f267da3444b3f6b85802fc9176def9bff0
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-04-01 22:17:19 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 140 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -2,49 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
         while (true) {
-            st = new StringTokenizer(br.readLine());
-            String text = st.nextToken();
-            if (text.equals("end")) break;
-
-            int con = 0;
-            int vow = 0;
-            int continue_vow = 0;
+            String input = br.readLine();
+            if (input.equals("end")) {
+                break;
+            }
+            int con = 0; int vow = 0; int continue_vow = 0;
             boolean err = false;
-            int N = text.length();
+            int n = input.length();
 
-            for (int i = 0; i<N; i++){
-                if (i>0 && text.charAt(i)==text.charAt(i-1)){
-                        if (text.charAt(i) != 'e' && text.charAt(i) != 'o'){
-                            err = true;
-                            break;
-                    }
-                }
-                if (vowels.contains(text.charAt(i))) {
-                    vow ++;
-                    continue_vow ++;
-                    con = 0;
-                    if (continue_vow==3){
+            for (int i=0; i<n; i++){
+                if (i>0 && input.charAt(i)==input.charAt(i-1)){
+                    if (input.charAt(i) != 'e' && input.charAt(i)!='o'){
                         err = true;
                         break;
                     }
-                }else {
+                }
+                if (vowels.contains(input.charAt(i))){
+                    vow ++; continue_vow ++;
+                    con = 0;
+                    if (continue_vow == 3){
+                        err = true;
+                        break;
+                    }
+                }
+                else{
                     con ++;
                     continue_vow = 0;
-                    if (con == 3){
+                    if (con==3){
                         err = true;
                         break;
                     }
                 }
             }
-            if (!err && vow>0) {
-                System.out.println("<" + text + "> is acceptable.");
-            } else {
-                System.out.println("<" + text + "> is not acceptable.");
+            if (!err && vow>0){
+                System.out.println("<" + input + "> is acceptable.");
+            }else{
+                System.out.println("<" + input + "> is not acceptable.");
             }
         }
     }
