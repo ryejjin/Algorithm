499b0c12ef07f08ed028b7269bfcb6261e5800e9
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-04-18 18:14:03 +0900

    [Silver I] Title: 문자열 교환, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int min = Integer.MAX_VALUE;
+        int len = 0;
+        for (int i=0; i<input.length(); i++){
+            if (input.charAt(i) == 'a') len ++;
+        }
+        for (int i=0; i<input.length(); i++){
+            int cnt = 0;
+            for (int j=i; j<len+i; j++){
+                if (input.charAt(j%input.length()) == 'b') cnt ++;
+            }
+            min = Math.min(min, cnt);
+        }
+        System.out.println(min);
+    }
+}
