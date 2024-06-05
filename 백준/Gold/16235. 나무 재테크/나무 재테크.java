9f2d5420a3079f6725fd57f77aa46f88ebeeef4d
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-06-05 17:50:34 +0900

    [Gold III] Title: 나무 재테크, Time: 1116 ms, Memory: 269044 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -2,88 +2,101 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, K, x, y, z, ans;
-    static int[][] nutrient, nNutrient;
-    static List<Integer>[][] soil;
+    static int N, M, K;
+    static int[][] nutrient;
+    static int[][] newNutrient;
+    static ArrayList<Integer>[][] soil;
     static int[] di = {-1, -1, -1, 0, 0, 1, 1, 1};
     static int[] dj = {-1, 0, 1, -1, 1, -1, 0, 1};
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        nutrient = new int[n][n];
-        nNutrient = new int[n][n];
-        soil = new ArrayList[n][n];
 
-        for (int i=0; i<n; i++){
-            Arrays.fill(nutrient[i], 5);
-            for (int j=0; j<n; j++){
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        nutrient = new int[N][N];
+        newNutrient = new int[N][N];
+        soil = new ArrayList[N][N];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(nutrient[i], 5); // 초기 양분은 모두 5
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                newNutrient[i][j] = Integer.parseInt(st.nextToken());
                 soil[i][j] = new ArrayList<>();
             }
         }
-        for (int i=0; i<n; i++){
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<n; j++){
-                nNutrient[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        for (int i=0; i<m; i++){
-            st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken())-1;
-            y = Integer.parseInt(st.nextToken())-1;
-            z = Integer.parseInt(st.nextToken());
-            soil[x][y] .add(z);
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int z = Integer.parseInt(st.nextToken());
+            soil[x][y].add(z);
         }
 
-        int new_season = 0;
-        while (new_season<K){
-            for (int i=0; i<n; i++){
-                for (int j=0; j<n; j++){
-                    List<Integer> trees = new ArrayList<>();
-                    int next_n = 0;
-                    Collections.sort(soil[i][j]);
-                    for (int k : soil[i][j]){
-                        if (nutrient[i][j]>=k){
-                            nutrient[i][j] -= k;
-                            trees.add(k+1);
-                        }else{
-                            next_n += (k/2);
-                        }
-                    }
-                    nutrient[i][j] += next_n;
-                    soil[i][j] = trees;
-                }
+        for (int year = 0; year < K; year++) {
+            springAndSummer();
+            fall();
+            winter();
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (ArrayList<Integer> trees : soil[i]) {
+                ans += trees.size();
             }
-            for (int i=0; i<n; i++){
-                for (int j=0; j<n; j++){
-                    for (int k=0; k<soil[i][j].size(); k++){
-                        if (soil[i][j].get(k)%5 == 0) {
-                            for (int w=0; w<8; w++){
-                                int ni = i+di[w]; int nj = j+dj[w];
-                                if (0<=ni && ni<n && 0<=nj && nj<n){
-                                    soil[ni][nj].add(0, 1);
-                                }
+        }
+
+        System.out.println(ans);
+    }
+
+    static void springAndSummer() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                Collections.sort(soil[i][j]);
+                ArrayList<Integer> newTrees = new ArrayList<>();
+                int deadTreesNutrient = 0;
+                for (int age : soil[i][j]) {
+                    if (nutrient[i][j] >= age) {
+                        nutrient[i][j] -= age;
+                        newTrees.add(age + 1);
+                    } else {
+                        deadTreesNutrient += age / 2;
+                    }
+                }
+                soil[i][j] = newTrees;
+                nutrient[i][j] += deadTreesNutrient;
+            }
+        }
+    }
+
+    static void fall() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int age : soil[i][j]) {
+                    if (age % 5 == 0) {
+                        for (int d = 0; d < 8; d++) {
+                            int ni = i + di[d];
+                            int nj = j + dj[d];
+                            if (ni >= 0 && ni < N && nj >= 0 && nj < N) {
+                                soil[ni][nj].add(0, 1);
                             }
                         }
                     }
                 }
             }
-            for (int i=0; i<n; i++){
-                for (int j=0; j<n; j++){
-                    nutrient[i][j] += nNutrient[i][j];
-                }
-            }
-
-            new_season++;
-
         }
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                ans += soil[i][j].size();
+    }
+
+    static void winter() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                nutrient[i][j] += newNutrient[i][j];
             }
         }
-        System.out.println(ans);
     }
 }
