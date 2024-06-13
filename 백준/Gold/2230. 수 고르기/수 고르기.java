d72b90b7fd2d9d123807b0b438692ccbc3187a38
례진 <129479166+sunsetzo@users.noreply.github.com>
2024-06-13 16:49:36 +0900

    [Gold V] Title: 수 고르기, Time: 360 ms, Memory: 27720 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int ans = Integer.MAX_VALUE;
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        int s=0; int e=0;
+        while (s<n){
+            if (arr[s]-arr[e]<m){
+                s++;
+                continue;
+            }
+            if (arr[s]-arr[e]==m){
+                ans=m;
+                break;
+            }
+
+            ans = Math.min(ans, arr[s]-arr[e]);
+            e++;
+        }
+        System.out.println(ans);
+    }
+}
